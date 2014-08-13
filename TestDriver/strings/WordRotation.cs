d6dc7ce0503e87c021d4ad9f9f917307fbb746ae
Dan Liu <danliu@microsoft.com>
2014-08-13 16:19:26 -0700

    add word rotation

---
+++ TestDriver/strings/WordRotation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDriver.Strings
+{
+    // Assume that you have a method isSubstring which checks if one word is a substring of another.
+    // Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring 
+    // (e.g., "waterbottle" is a rotation of "erbottlewat")
+    public static class WordRotation
+    {
+        public static bool IsWordRotation (string a, string b)
+        {
+            return false;
+        }
+    }
+}
