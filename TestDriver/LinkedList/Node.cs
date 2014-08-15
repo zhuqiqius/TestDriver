afabee3c85bf591f20da1f5618a92643764650ee
Dan Liu <zhuqiqius@hotmail.com>
2014-08-14 20:11:54 -0700

    add linked list

---
+++ TestDriver/LinkedList/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDriver.LinkedList
+{
+    public class Node
+    {
+        Node next = null;
+        int data;
+
+        public Node(int d)
+        {
+            data = d;
+        }
+        void appendToTail(int d)
+        {
+            Node end = new Node(d);
+            Node n = this;
+            while (n.next != null)
+            {
+                n = n.next;
+            }
+            n.next = end;
+        }
+    }
+}
---
+++ TestDriver/LinkedList/RemoveDuplicates.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDriver.LinkedList
+{
+    // Write code to remove duplicates from an unsorted linked list.
+    // How would you solve this problem if a temporary buffer is not allowed?
+    public static class RemoveDuplicates
+    {
+        public static Node RemoveDuplicatesFromUnSortedLinkedList(Node head)
+        {
+            return head;
+        }
+    }
+}
+
+
